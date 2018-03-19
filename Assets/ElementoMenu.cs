1522e3864b10942d4c49d61a96638cf78ac75ee1
Raul Felipe Blanco <ocdshiro@gmail.com>
2018-03-19 17:15:15 -0500

    Proyecto que por alguna razon no pareceio antes

---
+++ Assets/ElementoMenu.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ElementoMenu : MonoBehaviour, ISelectHandler, IDeselectHandler {
+
+	public Button padre;
+
+	public Button hijo;
+
+	public GameObject panelHijo;
+
+	public GameObject panelMio;
+
+	bool activo = false;
+
+	EventSystem evSys;
+	// Use this for initialization
+	void Start () {
+		Button btn = GetComponent<Button>();
+        if(btn != null)
+        {
+            btn.onClick.AddListener(Siguiente);
+        }
+        evSys = EventSystem.current;
+    }
+
+    void Update()
+    {
+    	if(activo && Input.GetKeyDown(KeyCode.Backspace))
+    	{
+    		Anterior();
+    	}
+    }
+
+    void Siguiente()
+    {
+		if(activo)
+		{
+			panelHijo.SetActive(true);
+			hijo.GetComponent<ElementoMenu>().padre = GetComponent<Button>();
+			evSys.SetSelectedGameObject(hijo.gameObject);
+		}
+    }
+
+    void Anterior()
+    {
+		if(activo && padre != null)
+		{
+			evSys.SetSelectedGameObject(padre.gameObject);
+			panelMio.SetActive(false);
+		}
+    }
+
+	public void OnSelect(BaseEventData eventData)
+    {
+        activo = true;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        activo = false;
+    }
+}
