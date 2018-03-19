e7950fc167341b31516a018683fbb738c24d0dc1
Juliana Montes <j.montes495@uniandes.edu.co>
2018-03-19 17:51:08 -0500

    corrección jerarquía

--- Assets/ElementoMenu.cs
+++ Assets/ElementoMenu.cs
@@ -6,18 +6,34 @@ using UnityEngine.UI;
 
 public class ElementoMenu : MonoBehaviour, ISelectHandler, IDeselectHandler {
 
+    // Botón del cuál fue llamado este elemento.
+    // Se necesita conocer para poder marcarlo como seleccionado en caso de
+    // retroceder en la jerarquía.
 	public Button padre;
 
+    // Botón que marcaré como seleccionado al mostrar el panel
+    // en el siguiente nivel de jerarquía. Sin esto no podré navegar al avanzar
+    // al siguiente nivel de jerarquía.
 	public Button hijo;
 
+    // Panel del siguiente nivel de jerarquía.
+    // Al conocerlo, puedo mostrarlo al avanzar al siguiente nivel de la jerarquía.
 	public GameObject panelHijo;
 
+    // Panel al que pertenezco.
+    // Al conocerlo, puedo ocultarlo al avanzar niveles a lo largo de la jerarquía.
 	public GameObject panelMio;
 
+    // Indica si el elemento es el que está actualmente elegido, para filtrar sus acciones.
 	bool activo = false;
 
+    // Sistema de eventos
 	EventSystem evSys;
-	// Use this for initialization
+
+	// Adiciona el comando de siguiente al botón.
+    // Este botón coincide en nivel de jerarquía. Es el botón al que pertenece este script.
+    // Inicializa el sistema de eventos, para poder marcar los botones como seleccionados
+    // transversalmente en la jerarquía.
 	void Start () {
 		Button btn = GetComponent<Button>();
         if(btn != null)
@@ -27,6 +43,7 @@ public class ElementoMenu : MonoBehaviour, ISelectHandler, IDeselectHandler {
         evSys = EventSystem.current;
     }
 
+    // En este caso, identifica si debe retrocederse en la jerarquía
     void Update()
     {
     	if(activo && Input.GetKeyDown(KeyCode.Backspace))
@@ -35,30 +52,42 @@ public class ElementoMenu : MonoBehaviour, ISelectHandler, IDeselectHandler {
     	}
     }
 
+    // Al estar activo el botón, avanza un paso en la jerarquía.
+    // Adicionalmente, establece al botón hijo como el elemento seleccionado.
     void Siguiente()
     {
 		if(activo)
 		{
 			panelHijo.SetActive(true);
-			hijo.GetComponent<ElementoMenu>().padre = GetComponent<Button>();
+            hijo.GetComponent<ElementoMenu>().padre = GetComponent<Button>();
 			evSys.SetSelectedGameObject(hijo.gameObject);
 		}
     }
 
+    // Al estar activo el botón, se devuelve un paso en la jerarquía.
+    // Adicionalmente, establece al botón padre como el elemento seleccionado.
     void Anterior()
     {
 		if(activo && padre != null)
 		{
 			evSys.SetSelectedGameObject(padre.gameObject);
-			panelMio.SetActive(false);
+			if(panelMio != null)
+            {
+                panelMio.SetActive(false);
+            }
 		}
     }
 
+    // Este método se llama al sombrearse un botón.
+    // Se marca el botón como el botón activo, para que sólo él pueda ejecutar 
+    // la acción de siguiente y anterior, sobre los demás scripts.
 	public void OnSelect(BaseEventData eventData)
     {
         activo = true;
     }
 
+    // Este método se llama al desmarcarse un botón.
+    // Al estar desmarcado, sus acciones no tienen efecto.
     public void OnDeselect(BaseEventData eventData)
     {
         activo = false;
